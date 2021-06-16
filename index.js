c997d758a63ebc724116f9fc730ede3808784afa
Delyan <dninov@gmail.com>
2021-06-16 08:56:22 +0300

    Added mongodb connection

--- index.js
+++ index.js
@@ -1,49 +1,23 @@
-//initialize express app
-//setup handlebars
-//setup static files
-//setup storage middleware
-//setup route handlers(controller actions)
-
 const express = require('express');
-const hbs = require('express-handlebars');
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
+const routesConfig = require('./config/routes');
 
-const { init: storage } = require('./models/storage');
+const { init: storage } = require('./services/storage');
 
-const { catalog } = require('./controllers/catalog');
-const { about } = require('./controllers/about');
-const { details } = require('./controllers/details');
-const { create, post: createPost } = require('./controllers/create');
-const { edit, post: editPost } = require('./controllers/edit');
-const { notFound } = require('./controllers/notFound');
 
 start();
 
 async function start(){
-
     const port = 3000;
     const app = express();
     
-    app.engine('hbs', hbs({
-        extname: '.hbs'
-    }));
-    
-    app.set('view engine', 'hbs');
-    app.use('/static', express.static('static'));
-    app.use('/js', express.static('js'));
-    app.use(express.urlencoded({extended: false}));
+    expressConfig(app);
+    await databaseConfig(app);
     
     app.use(await storage());
-    
-    app.get('/', catalog);
-    app.get('/about', about);
-    app.get('/details/:id', details);
-    app.get('/create', create);
-    app.post('/create', createPost);
-    
-    app.get('/edit/:id', edit);
-    app.post('/edit/:id', editPost);
+    routesConfig(app);
 
-    app.all('*', notFound)
     
     app.listen(port, () => console.log(`Server listening on port ${port}`));    
 }
---
+++ services/storage.js
@@ -0,0 +1,93 @@
+const Cube = require('../models/Cube')
+const fs = require('fs/promises');
+const uniqid = require('uniqid');
+//load and parse data file
+//provide ability to
+//-read all entries
+//-read single entry by ID
+//-add new entry
+// * get matching entries by search criteria
+
+let data = {};
+
+/*Model structure 
+    {
+        "name": "string",
+        "description": "string",
+        "imageUrl": "string",
+        "difficulty": "number"
+    }
+*/
+
+async function init(){
+    try{
+        data = JSON.parse(await fs.readFile('./models/data.json'));
+    }catch(err){
+        console.log('Error reading database');
+    }
+    return(req, res, next) =>{
+        req.storage = {
+            getAll,
+            getById,
+            create,
+            edit
+        };
+        next();
+    };
+}
+
+async function getAll(query){
+    let cubes = Object
+        .entries(data)
+        .map(([id, v])=>Object.assign({}, {id}, v));
+
+    if(query.search){
+        cubes = cubes.filter(c=> c.name.toLowerCase().includes(query.search.toLowerCase()));
+    }
+   
+    if(query.from){
+        cubes = cubes.filter(c=> c.difficulty >= query.from);
+    } 
+    if(query.to){
+        cubes = cubes.filter(c=> c.difficulty <= query.to);
+    } 
+    return cubes;
+}
+
+async function getById(id) {
+    const cube = data[id];
+    if(cube){
+        return Object.assign({}, {id}, cube );
+    }else{
+        return undefined;
+    }
+    return ;
+}
+
+async function create(cube){
+    const record = new Cube(cube);
+    return record.save();
+}
+
+async function edit(id, cube){
+    if(!data[id]){
+        throw new ReferenceError('No such ID in database');
+    }
+    data[id] = cube;
+    await persist();
+}
+async function persist(){
+    try{
+        await fs.writeFile('./models/data.json', JSON.stringify(data, null, 2));
+        console.log('>>> created new cube');
+     }catch(err){
+         console.error('Error writing out database');
+     }
+}
+
+module.exports = {
+    init,
+    getAll,
+    getById,
+    create
+};
